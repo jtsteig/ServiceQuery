cadf6896e343f6e5215efa4a36ef9a90bdc4bb8b
Jesse <steigj@gmail.com>
2020-07-16 19:22:12 -0600

    adding the entire create when creating.

--- src/service/serviceservice.py
+++ src/service/serviceservice.py
@@ -132,4 +132,48 @@ class ServicesService:
             review_rating=review_rating,
         )
         service = serviceRepo.Create(session)
+
+        for city in createService.operating_cities:
+            cityRepo = Cities(
+                service_id=service.id,
+                city_name=city
+            )
+            cityRepo.Create(session)
+
+        for review in createService.reviews:
+            reviewRepo = Reviews(
+                review_rating=review.review_rating,
+                review_comment=review.review_comment,
+                service_id=service.id
+            )
+            reviewRepo.Create(session)
+
+        for job in createService.work_types:
+            jobRepo = Jobs(
+                service_id=service.id,
+                job_name=job
+            )
+            jobRepo.Create(session)
+
+        for hours in createService.business_hours:
+            hourRepo = Hours(
+                service_id=service.id,
+                day_of_week=hours.day_of_week,
+                open_at=hours.open_at,
+                close_at=hours.close_at
+            )
+            hourRepo.Create(session)
+
+        addressRepo = Addresses(
+            service_id=service.id,
+            address_line_1=createService.business_address.address_line_1,
+            address_line_2=createService.business_address.address_line_2,
+            city=createService.business_address.city,
+            state_abbreviation=createService
+            .business_address
+            .state_abbreviation,
+            postal=createService.business_address.postal
+        )
+        addressRepo.Create(session)
+
         return self.GetModelForService(service, session)
